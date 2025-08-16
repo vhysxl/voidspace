83b241001a98c4032b7e46feccb2e823a12bd6f7
Yogi Ferdiansyah <yogif346@gmail.com>
2025-08-16 21:12:14 +0700

    finishing post crud

--- services/posts/internal/domain/post_domain.go
+++ services/posts/internal/domain/post_domain.go
@@ -5,17 +5,6 @@ import (
 	"time"
 )
 
-type FeedParams struct {
-	Cursor string
-	Limit  int32
-}
-
-type FeedResponse struct {
-	Posts      []*Post
-	NextCursor string
-	HasMore    bool
-}
-
 type Post struct {
 	ID         int32
 	Content    string
@@ -31,9 +20,9 @@ type PostRepository interface {
 	GetByID(ctx context.Context, id int32) (*Post, error)
 	Update(ctx context.Context, post *Post) error
 	Delete(ctx context.Context, id int32) error
-	GetAllPosts(ctx context.Context, userID int32) ([]*Post, error)
+	GetAllUserPosts(ctx context.Context, userID int32) ([]*Post, error)
 
-	// Feed methods
-	GetGlobalFeed(ctx context.Context, params FeedParams) (*FeedResponse, error)
-	GetFeedByUserIDs(ctx context.Context, userIDs []int32, params FeedParams) (*FeedResponse, error)
+	// Feed
+	GetGlobalFeed(ctx context.Context, limit, offset int32) ([]*Post, bool, error)
+	GetFollowFeed(ctx context.Context, userIDs []int32, limit, offset int32) ([]*Post, bool, error)
 }
--- services/posts/internal/repository/post_repository.go
+++ services/posts/internal/repository/post_repository.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"voidspace/posts/internal/domain"
+
+	"github.com/lib/pq"
 )
 
 type postRepository struct {
@@ -33,18 +35,8 @@ func (p *postRepository) Create(ctx context.Context, post *domain.Post) error {
 	return err
 }
 
-// Delete implements domain.PostRepository.
-func (p *postRepository) Delete(ctx context.Context, id int32) error {
-	_, err := p.db.ExecContext(
-		ctx,
-		`DELETE FROM posts WHERE id = $1`,
-		id,
-	)
-	return err
-}
-
 // GetAllPosts implements domain.PostRepository.
-func (p *postRepository) GetAllPosts(ctx context.Context, userID int32) ([]*domain.Post, error) {
+func (p *postRepository) GetAllUserPosts(ctx context.Context, userID int32) ([]*domain.Post, error) {
 	rows, err := p.db.QueryContext(
 		ctx,
 		`SELECT id, content, user_id, post_images, likes_count, created_at, updated_at
@@ -81,6 +73,97 @@ func (p *postRepository) GetAllPosts(ctx context.Context, userID int32) ([]*doma
 	return posts, nil
 }
 
+// GetFollowFeed implements domain.PostRepository.
+func (p *postRepository) GetFollowFeed(ctx context.Context, userIDs []int32, limit int32, offset int32) ([]*domain.Post, bool, error) {
+	rows, err := p.db.QueryContext(
+		ctx,
+		`SELECT id, content, user_id, post_images, likes_count, created_at, updated_at
+         FROM posts 
+         WHERE user_id = ANY($1) 
+         ORDER BY created_at DESC 
+         LIMIT $2 OFFSET $3`,
+		pq.Array(userIDs), limit+1, offset)
+	if err != nil {
+		return nil, false, err
+	}
+	defer rows.Close()
+
+	var posts []*domain.Post
+	for rows.Next() {
+		post := &domain.Post{}
+		err := rows.Scan(
+			&post.ID,
+			&post.Content,
+			&post.UserID,
+			&post.PostImages,
+			&post.LikesCount,
+			&post.CreatedAt,
+			&post.UpdatedAt,
+		)
+		if err != nil {
+			return nil, false, err
+		}
+		posts = append(posts, post)
+	}
+
+	hasMore := len(posts) > int(limit)
+	if hasMore {
+		posts = posts[:limit]
+	}
+
+	return posts, hasMore, nil
+}
+
+// GetGlobalFeed implements domain.PostRepository.
+func (p *postRepository) GetGlobalFeed(ctx context.Context, limit, offset int32) ([]*domain.Post, bool, error) {
+	rows, err := p.db.QueryContext(
+		ctx,
+		`SELECT id, content, user_id, post_images, likes_count, created_at, updated_at
+         FROM posts 
+         ORDER BY created_at DESC 
+         LIMIT $1 OFFSET $2`,
+		limit+1, offset)
+	if err != nil {
+		return nil, false, err
+	}
+	defer rows.Close()
+
+	var posts []*domain.Post
+	for rows.Next() {
+		post := &domain.Post{}
+		err := rows.Scan(
+			&post.ID,
+			&post.Content,
+			&post.UserID,
+			&post.PostImages,
+			&post.LikesCount,
+			&post.CreatedAt,
+			&post.UpdatedAt,
+		)
+		if err != nil {
+			return nil, false, err
+		}
+		posts = append(posts, post)
+	}
+
+	hasMore := len(posts) > int(limit)
+	if hasMore {
+		posts = posts[:limit]
+	}
+
+	return posts, hasMore, nil
+}
+
+// Delete implements domain.PostRepository.
+func (p *postRepository) Delete(ctx context.Context, id int32) error {
+	_, err := p.db.ExecContext(
+		ctx,
+		`DELETE FROM posts WHERE id = $1`,
+		id,
+	)
+	return err
+}
+
 // GetByID implements domain.PostRepository.
 func (p *postRepository) GetByID(ctx context.Context, id int32) (*domain.Post, error) {
 	post := &domain.Post{}
@@ -106,17 +189,16 @@ func (p *postRepository) GetByID(ctx context.Context, id int32) (*domain.Post, e
 	return post, nil
 }
 
-// GetFeedByUserIDs implements domain.PostRepository.
-func (p *postRepository) GetFeedByUserIDs(ctx context.Context, userIDs []int32, params domain.FeedParams) (*domain.FeedResponse, error) {
-	panic("unimplemented")
-}
-
-// GetGlobalFeed implements domain.PostRepository.
-func (p *postRepository) GetGlobalFeed(ctx context.Context, params domain.FeedParams) (*domain.FeedResponse, error) {
-	panic("unimplemented")
-}
-
 // Update implements domain.PostRepository.
 func (p *postRepository) Update(ctx context.Context, post *domain.Post) error {
-	panic("unimplemented")
+	_, err := p.db.ExecContext(
+		ctx,
+		`UPDATE posts SET content = $1, post_images = $2, updated_at = $3
+    WHERE id = $4`,
+		post.Content,
+		post.PostImages,
+		post.UpdatedAt,
+		post.ID,
+	)
+	return err
 }
