6bc6954992cd9e6e052570d2b16cdca0be1ea170
Yogi Ferdiansyah <yogif346@gmail.com>
2025-09-26 08:20:56 +0700

    fix cicd

---
+++ gateway/bootstrap/connection.go
@@ -0,0 +1,34 @@
+package bootstrap
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	grpcInsecure "google.golang.org/grpc/credentials/insecure"
+)
+
+// NewConn creates a new gRPC connection.
+// host should be of the form domain:port, e.g., example.com:443
+func NewConn(host string, insecure bool) (*grpc.ClientConn, error) {
+	var opts []grpc.DialOption
+	if host != "" {
+		opts = append(opts, grpc.WithAuthority(host))
+	}
+
+	if insecure {
+		opts = append(opts, grpc.WithTransportCredentials(grpcInsecure.NewCredentials()))
+	} else {
+		systemRoots, err := x509.SystemCertPool()
+		if err != nil {
+			return nil, err
+		}
+		cred := credentials.NewTLS(&tls.Config{
+			RootCAs: systemRoots,
+		})
+		opts = append(opts, grpc.WithTransportCredentials(cred))
+	}
+
+	return grpc.NewClient(host, opts...)
+}
