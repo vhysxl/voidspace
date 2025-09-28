a823757807482b42feb4a226a024b07c360ab363
Yogi Ferdiansyah <yogif346@gmail.com>
2025-09-29 03:02:10 +0700

    fix docs

--- gateway/middleware/api_middleware.go
+++ gateway/middleware/api_middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 	"voidspaceGateway/internal/api/responses"
 	"voidspaceGateway/internal/constants"
@@ -12,6 +13,7 @@ func ApiMiddleware(apiKey string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			receivedKey := c.Request().Header.Get("x-api-key")
+			fmt.Println(receivedKey == apiKey)
 			if receivedKey != apiKey {
 				return responses.ErrorResponseMessage(c, http.StatusUnauthorized, constants.ErrUnauthorized)
 			}
